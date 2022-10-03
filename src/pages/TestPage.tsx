8fc67c3588238c376a0c185d13e21033dff0eb47
luoxupan <luoxupan@didiglobal.com>
2022-10-03 15:50:43 +0800

    save

--- src/pages/TestPage.tsx
+++ src/pages/TestPage.tsx
@@ -1,6 +1,12 @@
 import * as React from "react";
+import { useSearchParams, useLocation } from 'react-router-dom';
 
 export default function TestPage() {
+
+  let location = useLocation();
+  let [searchParams, setSearchParams] = useSearchParams();
+  console.log('useSearchParams:', searchParams.get('id'));
+  console.log('location:', location);
 
   return (
     <div id="test-page">
