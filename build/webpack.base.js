39e9d8d0fce35d9626f80de218cb2030a28a2d01
luoxupan <luoxupan@didiglobal.com>
2022-10-04 18:58:27 +0800

    干掉source map

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -86,7 +86,7 @@ const Base = {
       state: path.resolve(__dirname, '../src/state/index'),
     },
   },
-  devtool: "hidden-source-map",
+  // devtool: "hidden-source-map",
 };
 
 module.exports = Base;
