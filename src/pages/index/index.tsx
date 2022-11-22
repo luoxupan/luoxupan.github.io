b678bdaf32ffaf3db80e9441b790b7d6b6f9bd34
luoxupan <luoxupan@didiglobal.com>
2022-11-22 19:23:36 +0800

    save

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,19 +1,34 @@
 import * as React from "react";
 import { useSearchParams, useLocation } from 'react-router-dom';
 import { State } from 'state';
+import { Button } from 'antd';
 import './index.less';
 
-export function IndexPage() {
+export function IndexPage(props: any) {
 
   let location = useLocation();
   let [searchParams, setSearchParams] = useSearchParams();
   console.log('useSearchParams:', searchParams.get('id'));
-  console.log('location:', location);
   const { state, dispatch } = React.useContext(State);
-  console.log('state:', state);
+  const [showName, setShowName] = React.useState(false);
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      // console.log('props:props', props)
+      console.log('props:', props.data.data_name);
+    }, 2000);
+  } ,[]);
 
   return (
     <div className="main-page">
+      {showName && <div>name: {props.props.name}</div>}
+      <Button
+        onClick={() => {
+          setShowName(true);
+        }}
+      >
+        Show Name
+      </Button>
       <div>首页</div>
     </div>
   );
