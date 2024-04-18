15bd6c7d22812155024e20f1d6b333c1d35dc22c
luoxupan <luoxupan@didiglobal.com>
2024-04-18 20:40:37 +0800

    indexDB

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -5,6 +5,7 @@ import { Button } from 'antd';
 import './index.less';
 import html2canvas from '../../html2canvas';
 import { useQuery } from '../../hooks';
+import { IndexDB } from '../../utils';
 
 export function IndexPage(props: any) {
 
@@ -41,6 +42,21 @@ export function IndexPage(props: any) {
         }}
       >
         点击Crash
+      </Button>
+      <Button
+        onClick={() => {
+          // @ts-ignore
+          window.IndexDB = IndexDB;
+          IndexDB.init().then(() => {
+            IndexDB.set('/optimusChannel/preLoadData', {
+              timestamp: Date.now(),
+              params: {},
+              response: {"status":0,"data":{"userInfo":{"uid":369436512018641,"uidStr":"369436512018641","cell":"00016111882","role":1,"email":null,"originId":1,"i18nInfo":{"countryCode":"+52","lang":null,"utcOffset":null},"emailStatus":null,"requesterType":1,"countryCodeNum":"+52"},"utcOffset":-360,"portalVerifyResult":true,"cityid":"52080200","pageList":[{"id":8,"name":"Viaje","icon":"http://img0.didiglobal.com/static/csglobal/optimus/nav_icon_ride_unsel_2x.png","businessModalityId":1,"userTypeId":1,"pageType":"3","defaultPage":true}],"orderSelectTitle":"Elige otro viaje","canonicalCountryCode":"MX","orderConfirmTitle":"Confirma el problema y el viaje"},"error":null,"errors":null,"traceId":"0a0f162f6620fc64475b75c90fb33302"},
+            });
+          });
+        }}
+      >
+        IndexDB设置数据
       </Button>
       <Button
         onClick={() => {
---
+++ src/utils/indexDB.ts
@@ -0,0 +1,150 @@
+const conf = {
+  db_name: 'help.didiglobal.com',
+  version: 1,
+  object_store: 'api-data',
+};
+
+export const isEqual = <TType>(x: TType, y: TType): boolean => {
+  if (Object.is(x, y)) return true
+  if (x instanceof Date && y instanceof Date) {
+    return x.getTime() === y.getTime()
+  }
+  if (x instanceof RegExp && y instanceof RegExp) {
+    return x.toString() === y.toString()
+  }
+  if (
+    typeof x !== 'object' ||
+    x === null ||
+    typeof y !== 'object' ||
+    y === null
+  ) {
+    return false
+  }
+  const keysX = Reflect.ownKeys(x as unknown as object) as (keyof typeof x)[]
+  const keysY = Reflect.ownKeys(y as unknown as object)
+  if (keysX.length !== keysY.length) return false
+  for (let i = 0; i < keysX.length; i++) {
+    if (!Reflect.has(y as unknown as object, keysX[i])) return false
+    if (!isEqual(x[keysX[i]], y[keysX[i]])) return false
+  }
+  return true
+}
+
+export class IndexDB {
+  static _db = undefined as any;
+  static _init_promise = undefined as any;
+  // 当前浏览器是否支持indexDB
+  static _index_db_support = 'indexedDB' in window ? true : false;
+  static init() {
+    // @ts-ignore
+    IndexDB._init_promise = new Promise((resolve: Function, reject: Function) => {
+      const request = window.indexedDB.open(conf.db_name, conf.version);
+      request.onerror = (event) => {
+        reject(event);
+      };
+      request.onupgradeneeded = (event: any) => {
+        const db = event.target.result;
+        if (!db.objectStoreNames.contains(conf.object_store)) {
+          db.createObjectStore(conf.object_store);
+        }
+      };
+      request.onsuccess = (event: any) => {
+        IndexDB._db = event.target.result;
+        resolve(IndexDB._db);
+      };
+    });
+    // @ts-ignore
+    window.IndexDB = IndexDB;
+    return IndexDB._init_promise;
+  }
+
+  static getObjectStore() {
+    return IndexDB._db.transaction([conf.object_store], "readwrite").objectStore(conf.object_store);
+  }
+
+  static setItem(key: string, data: { params: any, response: any }) {
+    return new Promise((resolve: Function, reject: Function) => {
+      IndexDB._init_promise.then(() => {
+        const request = IndexDB.getObjectStore().put({
+          ...data,
+          isIndexDBCache: true,
+          timestamp: Date.now(),
+        }, key);
+        request.onerror = (e: any) => {
+          reject(e);
+        };
+        request.onsuccess = (e: any) => {
+          resolve(e);
+        };
+      });
+    });
+  }
+  
+  static getItem(key: string,) {
+    return new Promise((resolve: Function, reject: Function) => {
+      IndexDB._init_promise.then(() => {
+        const request = IndexDB.getObjectStore().get(key);
+        request.onerror = (e: any) => {
+          reject(e);
+        };
+        request.onsuccess = (e: any) => {
+          resolve(e.target.result);
+        };
+      });
+    });
+  }
+
+  static deleteItem(key: string) {
+    return new Promise((resolve: Function, reject: Function) => {
+      IndexDB._init_promise.then(() => {
+        const request = IndexDB.getObjectStore().delete(key);
+        request.onerror = (e: any) => {
+          reject(e);
+        };
+        request.onsuccess = (e: any) => {
+          resolve(e);
+        };
+      });
+    });
+  }
+
+  // 接口数据获取
+  static get(key: string, params: any) {
+    return new Promise((resolve: Function, reject: Function) => {
+      const timeblock = 1 * 60 * 60 * 1000; // 一小时的毫秒数
+      const datanow = Date.now();
+      IndexDB.getItem(key).then((result: any) => {
+        const effective = (datanow - result.timestamp) < timeblock;
+        if (effective && isEqual(result.params, params)) {
+          resolve(result.response);
+        } else {
+          resolve(undefined);
+        }
+      });
+    });
+  }
+
+  static race(req_1: any, req_2: any) {
+    return new Promise((resolve: Function, reject: Function) => {
+      Promise.race([req_1, req_2]).then((res: any) => {
+        console.log('res:', res);
+        if (res) {
+          resolve(res);
+        } else {
+          resolve(req_2);
+        }
+      });
+    });
+  }
+}
+
+/**
+ * 使用示例
+  IndexDB.get('/optimusChannel/preLoadData', params).then((response) => {
+    if (response) {
+      return response;
+    } else {
+      return Http.post('/optimusChannel/preLoadData', { ...params, })
+    }
+  });
+ */
